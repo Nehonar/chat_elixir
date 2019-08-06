7b16461164c238cf05b5d13e0dd1ac5566fa84f5
nehonar <benito.avar@gmail.com>
2019-08-06 11:06:59 +0200

    first commit

---
+++ lib/chat_elixir.ex
@@ -0,0 +1,18 @@
+defmodule ChatElixir do
+  @moduledoc """
+  Documentation for ChatElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChatElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
